7b3e1f0762b2822c76434dfd1fa46cf70a50fe17
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-11 19:04:09 +0200

    deprecated bin command. Switched to using @creuna/cli in vs code tasks

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
-const newProject = require('../index');
+/* eslint-env node */
+/* eslint-disable no-console */
+const chalk = require('chalk');
 
-newProject(process.argv[2]);
+const blue = chalk.blueBright;
+const cyan = chalk.cyan;
+
+console.log(
+  `🤗  Hey! ${blue('creuna-new')} has been deprecated. Use ${blue(
+    '@creuna/cli'
+  )} instead! It's much nicer.
+  • ${blue('yarn remove -g @creuna/create-react-app')} or ${cyan(
+    'npm uninstall -g @creuna/create-react-app'
+  )}
+  • ${blue('yarn add -g @creuna/cli')} or ${cyan('npm install -g @creuna/cli')}
+  • ${blue('creuna')}
+  `
+);
