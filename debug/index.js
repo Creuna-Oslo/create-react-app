d4213308f9529b6e72e0ee89629a2fbdbe5df1ef
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-22 22:27:13 +0200

    Removes prompt and logging. Now accepts input as options object

--- debug/index.js
+++ debug/index.js
@@ -1,7 +1,33 @@
-const createApp = require('../index');
+/* eslint-env node */
+/* eslint-disable no-console */
+const chalk = require('chalk');
 
-createApp(process.argv[2]).then(({ messages }) => {
-  messages.forEach(({ text }) => {
-    console.log(text);
-  });
-});
+const { canWriteFiles, writeFiles } = require('../index');
+
+const logError = text => {
+  console.log(`🚫  ${chalk.redBright(text)}`);
+  process.exit(1);
+};
+
+const logMessage = ({ emoji, text }) => {
+  console.log(`${emoji ? `${emoji} ` : ''} ${text}`);
+};
+
+canWriteFiles(process.argv[2])
+  .then(async () => {
+    writeFiles({
+      authorEmail: 'john.doe@email.com',
+      authorName: 'John Doe',
+      projectName: 'my-project',
+      projectPath: process.argv[2],
+      useApiHelper: true,
+      useAnalyticsHelper: true,
+      useMessenger: true,
+      useResponsiveImages: true
+    })
+      .then(({ messages }) => {
+        messages.forEach(logMessage);
+      })
+      .catch(logError);
+  })
+  .catch(logError);
--- index.js
+++ index.js
@@ -4,165 +4,115 @@ const chalk = require('chalk');
 const fs = require('fs');
 const { copySync, ensureDirSync } = require('fs-extra');
 const path = require('path');
-const prompt = require('@creuna/prompt');
 
+const canWriteFiles = require('./utils/can-write-files');
 const createApiHelper = require('./templates/api-helper/create-api-helper');
 const createAppComponent = require('./templates/app-component/create-app-component');
 const createHomeComponent = require('./templates/home-component/create-home-component');
 const createPackageJson = require('./templates/package-json/create-package-json');
 const filterFiles = require('./utils/filter-files');
 
-module.exports = function(projectPath) {
-  return new Promise(async resolve => {
-    const isAbsolutePath =
-      projectPath && (projectPath[0] === '~' || projectPath[0] === '/');
-
-    if (isAbsolutePath) {
-      console.log(
-        `🚫  create-creuna-react-app ${chalk.redBright(
-          'does not support absolute paths. Aborting'
-        )}`
-      );
-      process.exit(1);
-    }
-
+function writeFiles({
+  authorEmail,
+  authorName,
+  projectName,
+  projectPath,
+  useApiHelper,
+  useAnalyticsHelper,
+  useMessenger,
+  useResponsiveImages
+}) {
+  return new Promise(async (resolve, reject) => {
     const buildDir = path.join(process.cwd(), projectPath || '');
     const templateDir = path.join(__dirname, 'templates');
 
-    // Check for preexisting files or folders
-    if (projectPath && fs.existsSync(buildDir)) {
-      // Abort if a directory is provided and it already exists to avoid overriding existing files
-      console.log(
-        `🚫  ${chalk.redBright('Directory already exists. Aborting')}`
-      );
-      process.exit(1);
-    } else if (!projectPath) {
-      // Abort if files or folders already exists in the current working directory
-      const filesInBuildDir = fs
-        .readdirSync(buildDir)
-        .filter(fileName => ['.DS_Store', '.git'].indexOf(fileName) === -1);
-
-      if (filesInBuildDir.length) {
-        console.log(
-          `🚫  ${chalk.redBright(
-            `There's already stuff in the current directory. Aborting`
-          )}`
-        );
-        console.log(chalk.redBright('Found the following stuff:'));
-        filesInBuildDir.forEach(fileName => {
-          console.log(`  ${chalk.redBright(fileName)}`);
-        });
-        process.exit(1);
-      }
+    try {
+      await canWriteFiles(projectPath);
+    } catch (errorMessage) {
+      return reject(errorMessage);
     }
 
-    const {
-      authorEmail,
-      authorName,
-      projectName,
-      useApiHelper,
-      useAnalyticsHelper,
-      useMessenger,
-      useResponsiveImages
-    } = await prompt({
-      projectName: {
-        text: '🚀  Project name (kebab-case)'
-      },
-      authorName: {
-        text: '😸  Your full name'
-      },
-      authorEmail: {
-        text: '💌  Your email address'
-      },
-      useApiHelper: {
-        text: '☁️  Include API-helper?',
-        type: Boolean
-      },
-      useMessenger: {
-        text: '💬  Include message helper for API?',
-        type: Boolean
-      },
-      useAnalyticsHelper: {
-        text: '📈  Include Analytics helper?',
-        type: Boolean
-      },
-      useResponsiveImages: {
-        text: '🖼️  Include responsive images helper?',
-        type: Boolean
-      }
-    });
+    // If the promise tries to catch errors asynchronously (in a .catch()), Node will output a 'UnhandledPromiseRejectionWarning' warning which hides the actual error in a wall of text and does not output a stack trace. Reject synchronously to avoid this
+    try {
+      // Make build directory if it doesn't exist
+      ensureDirSync(buildDir);
 
-    // Make build directory if it doesn't exist
-    ensureDirSync(buildDir);
+      copySync(path.join(__dirname, 'static-files'), buildDir, {
+        filter: filterFiles.bind(null, {
+          useAnalyticsHelper,
+          useMessenger,
+          useResponsiveImages
+        })
+      });
 
-    copySync(path.join(__dirname, 'static-files'), buildDir, {
-      filter: filterFiles.bind(null, {
-        useAnalyticsHelper,
-        useMessenger,
-        useResponsiveImages
-      })
-    });
-
-    // package.json
-    fs.writeFileSync(
-      path.join(buildDir, 'package.json'),
-      createPackageJson({
-        authorEmail,
-        authorName,
-        projectName,
-        useAnalyticsHelper,
-        useMessenger
-      })
-    );
-
-    // eslintrc
-    // This was moved to 'templates' because it messed up autoformatting when editing files in 'static-files'
-    fs.copyFileSync(
-      path.join(templateDir, 'eslintrc/.eslintrc.json'),
-      path.join(buildDir, '.eslintrc.json')
-    );
-
-    // api-helper
-    if (useApiHelper) {
+      // package.json
       fs.writeFileSync(
-        path.join(buildDir, 'source/js/api-helper.js'),
-        createApiHelper({ useAnalyticsHelper, useMessenger })
+        path.join(buildDir, 'package.json'),
+        createPackageJson({
+          authorEmail,
+          authorName,
+          projectName,
+          useAnalyticsHelper,
+          useMessenger
+        })
       );
+
+      // eslintrc
+      // This was moved to 'templates' because it messed up autoformatting when editing files in 'static-files'
+      fs.copyFileSync(
+        path.join(templateDir, 'eslintrc/.eslintrc.json'),
+        path.join(buildDir, '.eslintrc.json')
+      );
+
+      // api-helper
+      if (useApiHelper) {
+        fs.writeFileSync(
+          path.join(buildDir, 'source/js/api-helper.js'),
+          createApiHelper({ useAnalyticsHelper, useMessenger })
+        );
+      }
+
+      // app.jsx
+      fs.writeFileSync(
+        path.join(buildDir, 'source/mockup/app.jsx'),
+        createAppComponent(projectName)
+      );
+
+      // home.jsx
+      fs.writeFileSync(
+        path.join(buildDir, 'source/mockup/pages/home.jsx'),
+        createHomeComponent(projectName)
+      );
+
+      return resolve({
+        buildDir,
+        messages: [
+          { emoji: '🦄', text: chalk.greenBright('All done!') },
+          { text: 'Next steps:' }
+        ]
+          .concat(
+            projectPath ? { text: chalk.blueBright(`• cd ${projectPath}`) } : []
+          )
+          .concat([
+            {
+              text: `• ${chalk.blueBright('yarn')} or ${chalk.cyan(
+                'npm install'
+              )} to install dependencies,`
+            },
+            {
+              text: `• ${chalk.blueBright('yarn dev')} or ${chalk.cyan(
+                'npm run dev'
+              )} to start working!\n`
+            }
+          ])
+      });
+    } catch (error) {
+      return reject(error.stack || error.message);
     }
-
-    // app.jsx
-    fs.writeFileSync(
-      path.join(buildDir, 'source/mockup/app.jsx'),
-      createAppComponent(projectName)
-    );
-
-    // home.jsx
-    fs.writeFileSync(
-      path.join(buildDir, 'source/mockup/pages/home.jsx'),
-      createHomeComponent(projectName)
-    );
-
-    return resolve({
-      buildDir,
-      messages: [
-        { emoji: '🦄', text: chalk.greenBright('All done!') },
-        { text: 'Next steps:' }
-      ]
-        .concat(
-          projectPath ? { text: chalk.blueBright(`• cd ${projectPath}`) } : []
-        )
-        .concat([
-          {
-            text: `• ${chalk.blueBright('yarn')} or ${chalk.cyan(
-              'npm install'
-            )} to install dependencies,`
-          },
-          {
-            text: `• ${chalk.blueBright('yarn dev')} or ${chalk.cyan(
-              'npm run dev'
-            )} to start working!\n`
-          }
-        ])
-    });
   });
+}
+
+module.exports = {
+  canWriteFiles,
+  writeFiles
 };
