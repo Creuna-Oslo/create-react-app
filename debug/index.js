99c555f3da575eb464649093d44680522a635b88
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-06-22 19:08:16 +0200

    Fix PR comment: replace callback with promise

--- debug/index.js
+++ debug/index.js
@@ -1,6 +1,6 @@
 const createApp = require('../index');
 
-createApp(process.argv[2], (buildDir, messages) => {
+createApp(process.argv[2]).then(({ messages }) => {
   messages.forEach(({ text }) => {
     console.log(text);
   });
--- index.js
+++ index.js
@@ -12,153 +12,157 @@ const createHomeComponent = require('./templates/home-component/create-home-comp
 const createPackageJson = require('./templates/package-json/create-package-json');
 const filterFiles = require('./utils/filter-files');
 
-module.exports = async function(projectPath, callback) {
-  const isAbsolutePath =
-    projectPath && (projectPath[0] === '~' || projectPath[0] === '/');
+module.exports = async function(projectPath) {
+  return new Promise(async resolve => {
+    const isAbsolutePath =
+      projectPath && (projectPath[0] === '~' || projectPath[0] === '/');
 
-  if (isAbsolutePath) {
-    console.log(
-      `🚫  create-creuna-react-app ${chalk.redBright(
-        'does not support absolute paths. Aborting'
-      )}`
-    );
-    process.exit(1);
-  }
-
-  const buildDir = path.join(process.cwd(), projectPath || '');
-  const templateDir = path.join(__dirname, 'templates');
-
-  // Check for preexisting files or folders
-  if (projectPath && fs.existsSync(buildDir)) {
-    // Abort if a directory is provided and it already exists to avoid overriding existing files
-    console.log(`🚫  ${chalk.redBright('Directory already exists. Aborting')}`);
-    process.exit(1);
-  } else if (!projectPath) {
-    // Abort if files or folders already exists in the current working directory
-    const filesInBuildDir = fs
-      .readdirSync(buildDir)
-      .filter(fileName => ['.DS_Store', '.git'].indexOf(fileName) === -1);
-
-    if (filesInBuildDir.length) {
+    if (isAbsolutePath) {
       console.log(
-        `🚫  ${chalk.redBright(
-          `There's already stuff in the current directory. Aborting`
+        `🚫  create-creuna-react-app ${chalk.redBright(
+          'does not support absolute paths. Aborting'
         )}`
       );
-      console.log(chalk.redBright('Found the following stuff:'));
-      filesInBuildDir.forEach(fileName => {
-        console.log(`  ${chalk.redBright(fileName)}`);
-      });
       process.exit(1);
     }
-  }
 
-  const {
-    authorEmail,
-    authorName,
-    projectName,
-    useApiHelper,
-    useAnalyticsHelper,
-    useMessenger,
-    useResponsiveImages
-  } = await prompt({
-    projectName: {
-      text: '🚀  Project name (kebab-case)'
-    },
-    authorName: {
-      text: '😸  Your full name'
-    },
-    authorEmail: {
-      text: '💌  Your email address'
-    },
-    useApiHelper: {
-      text: '☁️  Include API-helper?',
-      type: Boolean
-    },
-    useMessenger: {
-      text: '💬  Include message helper for API?',
-      type: Boolean
-    },
-    useAnalyticsHelper: {
-      text: '📈  Include Analytics helper?',
-      type: Boolean
-    },
-    useResponsiveImages: {
-      text: '🖼️  Include responsive images helper?',
-      type: Boolean
+    const buildDir = path.join(process.cwd(), projectPath || '');
+    const templateDir = path.join(__dirname, 'templates');
+
+    // Check for preexisting files or folders
+    if (projectPath && fs.existsSync(buildDir)) {
+      // Abort if a directory is provided and it already exists to avoid overriding existing files
+      console.log(
+        `🚫  ${chalk.redBright('Directory already exists. Aborting')}`
+      );
+      process.exit(1);
+    } else if (!projectPath) {
+      // Abort if files or folders already exists in the current working directory
+      const filesInBuildDir = fs
+        .readdirSync(buildDir)
+        .filter(fileName => ['.DS_Store', '.git'].indexOf(fileName) === -1);
+
+      if (filesInBuildDir.length) {
+        console.log(
+          `🚫  ${chalk.redBright(
+            `There's already stuff in the current directory. Aborting`
+          )}`
+        );
+        console.log(chalk.redBright('Found the following stuff:'));
+        filesInBuildDir.forEach(fileName => {
+          console.log(`  ${chalk.redBright(fileName)}`);
+        });
+        process.exit(1);
+      }
     }
-  });
 
-  // Make build directory if it doesn't exist
-  ensureDirSync(buildDir);
-
-  copySync(path.join(__dirname, 'static-files'), buildDir, {
-    filter: filterFiles.bind(null, {
-      useAnalyticsHelper,
-      useMessenger,
-      useResponsiveImages
-    })
-  });
-
-  // package.json
-  fs.writeFileSync(
-    path.join(buildDir, 'package.json'),
-    createPackageJson({
+    const {
       authorEmail,
       authorName,
       projectName,
+      useApiHelper,
       useAnalyticsHelper,
-      useMessenger
-    })
-  );
+      useMessenger,
+      useResponsiveImages
+    } = await prompt({
+      projectName: {
+        text: '🚀  Project name (kebab-case)'
+      },
+      authorName: {
+        text: '😸  Your full name'
+      },
+      authorEmail: {
+        text: '💌  Your email address'
+      },
+      useApiHelper: {
+        text: '☁️  Include API-helper?',
+        type: Boolean
+      },
+      useMessenger: {
+        text: '💬  Include message helper for API?',
+        type: Boolean
+      },
+      useAnalyticsHelper: {
+        text: '📈  Include Analytics helper?',
+        type: Boolean
+      },
+      useResponsiveImages: {
+        text: '🖼️  Include responsive images helper?',
+        type: Boolean
+      }
+    });
 
-  // eslintrc
-  // This was moved to 'templates' because it messed up autoformatting when editing files in 'static-files'
-  fs.copyFileSync(
-    path.join(templateDir, 'eslintrc/.eslintrc.json'),
-    path.join(buildDir, '.eslintrc.json')
-  );
+    // Make build directory if it doesn't exist
+    ensureDirSync(buildDir);
 
-  // api-helper
-  if (useApiHelper) {
+    copySync(path.join(__dirname, 'static-files'), buildDir, {
+      filter: filterFiles.bind(null, {
+        useAnalyticsHelper,
+        useMessenger,
+        useResponsiveImages
+      })
+    });
+
+    // package.json
     fs.writeFileSync(
-      path.join(buildDir, 'source/js/api-helper.js'),
-      createApiHelper({ useAnalyticsHelper, useMessenger })
+      path.join(buildDir, 'package.json'),
+      createPackageJson({
+        authorEmail,
+        authorName,
+        projectName,
+        useAnalyticsHelper,
+        useMessenger
+      })
     );
-  }
 
-  // app.jsx
-  fs.writeFileSync(
-    path.join(buildDir, 'source/mockup/app.jsx'),
-    createAppComponent(projectName)
-  );
+    // eslintrc
+    // This was moved to 'templates' because it messed up autoformatting when editing files in 'static-files'
+    fs.copyFileSync(
+      path.join(templateDir, 'eslintrc/.eslintrc.json'),
+      path.join(buildDir, '.eslintrc.json')
+    );
 
-  // home.jsx
-  fs.writeFileSync(
-    path.join(buildDir, 'source/mockup/pages/home.jsx'),
-    createHomeComponent(projectName)
-  );
+    // api-helper
+    if (useApiHelper) {
+      fs.writeFileSync(
+        path.join(buildDir, 'source/js/api-helper.js'),
+        createApiHelper({ useAnalyticsHelper, useMessenger })
+      );
+    }
 
-  callback(
-    buildDir,
-    [
-      { emoji: '🦄', text: chalk.greenBright('All done!') },
-      { text: 'Next steps:' }
-    ]
-      .concat(
-        projectPath ? { text: chalk.blueBright(`• cd ${projectPath}`) } : []
-      )
-      .concat([
-        {
-          text: `• ${chalk.blueBright('yarn')} or ${chalk.cyan(
-            'npm install'
-          )} to install dependencies,`
-        },
-        {
-          text: `• ${chalk.blueBright('yarn dev')} or ${chalk.cyan(
-            'npm run dev'
-          )} to start working!\n`
-        }
-      ])
-  );
+    // app.jsx
+    fs.writeFileSync(
+      path.join(buildDir, 'source/mockup/app.jsx'),
+      createAppComponent(projectName)
+    );
+
+    // home.jsx
+    fs.writeFileSync(
+      path.join(buildDir, 'source/mockup/pages/home.jsx'),
+      createHomeComponent(projectName)
+    );
+
+    return resolve({
+      buildDir,
+      messages: [
+        { emoji: '🦄', text: chalk.greenBright('All done!') },
+        { text: 'Next steps:' }
+      ]
+        .concat(
+          projectPath ? { text: chalk.blueBright(`• cd ${projectPath}`) } : []
+        )
+        .concat([
+          {
+            text: `• ${chalk.blueBright('yarn')} or ${chalk.cyan(
+              'npm install'
+            )} to install dependencies,`
+          },
+          {
+            text: `• ${chalk.blueBright('yarn dev')} or ${chalk.cyan(
+              'npm run dev'
+            )} to start working!\n`
+          }
+        ])
+    });
+  });
 };
